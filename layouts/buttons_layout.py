65ac1e3c2e06eb7e3dd7aeefdeee742e438b959c
shubham65p <shubhamparashar65000@gmail.com>
2026-01-08 21:36:51 +0530

    added custom button

--- layouts/buttons_layout.py
+++ layouts/buttons_layout.py
@@ -5,7 +5,7 @@ from patient_dialog import PatientDialog
 from database_manager import DatabaseManager
 # from pages.patient_page import PatientPage
 from PySide6.QtCore import Signal
-
+from button.custom_button import CustomButton
 
 class ButtonsLayout(QWidget):
     def __init__(self) -> None:
@@ -14,65 +14,70 @@ class ButtonsLayout(QWidget):
         layout = QHBoxLayout(self)
         self.db = DatabaseManager()
         # self.patient_page = PatientPage()
-        self.add_btn = QPushButton("Add Patient")
-        self.add_btn.setStyleSheet("""
-        QPushButton {
-            background-color: #16a34a;
-            color: white;
-            border: none;
-            padding: 8px 14px;
-            border-radius: 6px;
-            font-weight: 600;
-        }
-        QPushButton:hover {
-            background-color: #15803d;
-        }
-        QPushButton:pressed {
-            background-color: #166534;
-        }
-        """)
+        self.add_btn = CustomButton("Add Patient", "#16a34a")
+        # self.add_btn = QPushButton("Add Patient")
+        # self.add_btn.setStyleSheet("""
+        # QPushButton {
+        #     background-color: #16a34a;
+        #     color: white;
+        #     border: none;
+        #     padding: 8px 14px;
+        #     border-radius: 6px;
+        #     font-weight: 600;
+        # }
+        # QPushButton:hover {
+        #     background-color: #15803d;
+        # }
+        # QPushButton:pressed {
+        #     background-color: #166534;
+        # }
+        # """)
         # self.add_btn.clicked.connect(self.add_clicked.emit)
         layout.addWidget(self.add_btn)
 
-        self.edit_btn = QPushButton("Edit Patient")
-        self.edit_btn.setStyleSheet("""
-            QPushButton {
-                background-color: #2563eb;
-                color: white;
-                border: none;
-                padding: 8px 14px;
-                border-radius: 6px;
-                font-weight: 600;
-            }
-            QPushButton:hover {
-                background-color: #1d4ed8;
-            }
-            QPushButton:pressed {
-                background-color: #1e40af;
-            }
-            """)
+        self.edit_btn = CustomButton("Edit Patient", "#2563eb")
+        # self.edit_btn = QPushButton("Edit Patient")
+        # self.edit_btn.setStyleSheet("""
+        #     QPushButton {
+        #         background-color: #2563eb;
+        #         color: white;
+        #         border: none;
+        #         padding: 8px 14px;
+        #         border-radius: 6px;
+        #         font-weight: 600;
+        #     }
+        #     QPushButton:hover {
+        #         background-color: #1d4ed8;
+        #     }
+        #     QPushButton:pressed {
+        #         background-color: #1e40af;
+        #     }
+        #     """)
         # edit_btn.clicked.connect(self.edit_patient)
         layout.addWidget(self.edit_btn)
-        
-        self.delete_btn = QPushButton("Delete Patient")
-        self.delete_btn.setStyleSheet("""
-            QPushButton {
-                background-color: #dc2626;
-                color: white;
-                border: none;
-                padding: 8px 14px;
-                border-radius: 6px;
-                font-weight: 600;
-            }
-            QPushButton:hover {
-                background-color: #b91c1c;
-            }
-            QPushButton:pressed {
-                background-color: #991b1b;
-            }
-            """)
+        self.delete_btn = CustomButton("Delete Patient", "#dc2626")
+        # self.delete_btn = QPushButton("Delete Patient")
+        # self.delete_btn.setStyleSheet("""
+        #     QPushButton {
+        #         background-color: #dc2626;
+        #         color: white;
+        #         border: none;
+        #         padding: 8px 14px;
+        #         border-radius: 6px;
+        #         font-weight: 600;
+        #     }
+        #     QPushButton:hover {
+        #         background-color: #b91c1c;
+        #     }
+        #     QPushButton:pressed {
+        #         background-color: #991b1b;
+        #     }
+        #     """)
         # delete_btn.clicked.connect(self.delete_patient)
         layout.addWidget(self.delete_btn)
+
+        # for btn in (self.add_btn, self.edit_btn, self.delete_btn):
+        #     btn.setCursor(Qt.CursorShape.PointingHandCursor)
 
         layout.addStretch()
         self.row_count_lable = QLabel("Total: 0")
